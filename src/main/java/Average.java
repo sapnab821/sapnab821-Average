14e27b7d69948dd06968299044d02f284a99fa14
sapnab821 <sapnabolikal@gmail.com>
2023-02-28 21:25:31 +0000

    Average

--- src/main/java/Average.java
+++ src/main/java/Average.java
@@ -18,6 +18,14 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        return 0;
+        int sum=0;
+        double finAv=0;
+        for(int i=0; i< nums.length; i++){
+            sum=sum+nums[i];
+            double finSum= (double) sum;
+            finAv=finSum/nums.length;
+
+        }
+        return finAv;
     }
 }
